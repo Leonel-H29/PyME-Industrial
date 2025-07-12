9e2c8c577e98dbed53dc8ab09d614af4adee8523
Celeste Mariel Soto <celestemairel28@gmail.com>
2025-07-11 22:40:42 -0300

    Creo la clase file

---
+++ Data/file.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+import csv
+
+class File:
+    __file : File
+    def __init__(self, path: str = ""):
+        self.path = path
+
+    def itemsFromFile(self):
+        items = []
+        try:
+            with open(self.path, mode='r', newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    items.append(row)
+        except FileNotFoundError:
+            print(f"Archivo no encontrado: {self.path}")
+        return items
+
+    def open(self):
+        try:
+            self.__file = open(self.path, mode='r', newline='', encoding='utf-8')
+            print("Archivo abierto.")
+        except FileNotFoundError:
+            print("Archivo no encontrado.")
+
+    def close(self):
+        if self.__file:
+            self.__file.close()
+            print("Archivo cerrado.")
+            self.__file = None
+
+    def get_path(self):
+        return self.path
+
+    def set_path(self, path: str):
+        self.path = path
