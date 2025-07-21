4a4d0e09c84123b762a1b7d0856fe4089713a8d7
Germán Kousal <germankousal@protonmail.com>
2025-07-21 20:23:04 -0300

    Agregado comando en CLI para limpiar pantalla. Al iniciar el el programa la pantalla se limpia automaticamente.

--- CLI/CLI.py
+++ CLI/CLI.py
@@ -1,3 +1,4 @@
+import os
 from cmd import Cmd
 
 
@@ -5,8 +6,29 @@ class MipymeShell(Cmd):
     intro = 'Bienvendio a la shell de MiPyME\n\nPara una lista completa de comandos ingrese "help" o "?"\n'
     prompt = '(MiPyME)/> '
 
+    # Accepted commands
+
+    def do_clear(self, arg):
+        """Limpia la pantalla del terminal."""
+        # Check the operating system and execute the appropriate command
+        if os.name == 'nt':  # For Windows
+            os.system('cls')
+        else:  # For Linux/macOS
+            os.system('clear')
+
+
     def do_exit(self, arg):
-        'Termina y sale del programa'
+        """Termina y sale del programa."""
         print('Finalizando MiPyME.')
         return True
+    
+
+    # Other methods
+
+    def preloop(self):
+        if os.name == 'nt':  # For Windows
+            os.system('cls')
+        else:  # For Linux/macOS
+            os.system('clear')
+
 
