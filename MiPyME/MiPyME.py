4e3f7c3f765b4057cffe9f301dfce41c09800a55
Germán Kousal <germankousal@protonmail.com>
2025-07-21 19:43:58 -0300

    Created boilerplate for MiPyME, the main orchestrator module, which orchestrates the message passing between modules, originating in the CLI interface or elsewhere.

---
+++ MiPyME/MiPyME.py
@@ -0,0 +1,9 @@
+"""
+This module orchestrates the interaction between the different modules on this software.
+It receives commands from the CLI module, executes them either by delegating the responsibility
+to other modules or by itself.
+"""
+
+class MiPyME:
+    pass
+
