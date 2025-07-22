aaa175af549c1f9431428aac728d8fb0ea88dbeb
Leonel Herrera <leonelherreradkno@gmail.com>
2025-07-22 12:06:29 -0300

    I've added a validation for emails in users

--- User/user.py
+++ User/user.py
@@ -1,3 +1,4 @@
+import re
 from Observer.observer import Observer
 
 
@@ -6,11 +7,17 @@ class User(Observer):
     __email: str
 
     def __init__(self, email: str):
-        self.__email: str = email
+        self.__email: str = self.__validate_email(email)
+
+    def __validate_email(self, email: str) -> str:
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if re.match(pattern, email) is None:
+            raise ValueError("El correo electrónico no es válido")
+        return email
 
     def update(self, item, message):
         print(
-            f"Notificando a {self.__email}: {message} (Item: {item}, Estado: {item.get_state()})")
+            f"Notificando a {self.__email}: {message} (\nItem: {item}, \nEstado: {item.get_state()}\n)")
 
     def get_email(self) -> str:
         return self.__email
