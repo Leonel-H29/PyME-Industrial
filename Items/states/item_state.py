028b4aaf592e6c1ea38e90fa8a4050a3a22b9f88
Leonel Herrera <leonelherreradkno@gmail.com>
2025-07-23 15:55:16 -0300

    I've removed the @abstractmethod decorator

--- Items/states/item_state.py
+++ Items/states/item_state.py
@@ -2,27 +2,22 @@ from abc import abstractmethod
 
 
 class ItemState:
-    @abstractmethod
+
     def quote(self, item) -> None:
         raise Exception("No se puede cotizar desde este estado.")
 
-    @abstractmethod
     def order(self, item) -> None:
         raise Exception("No se puede ordenar desde este estado.")
 
-    @abstractmethod
     def transport(self, item) -> None:
         raise Exception("No se puede cancelar desde este estado.")
 
-    @abstractmethod
     def receive(self, item) -> None:
         raise Exception("No se puede transportar desde este estado.")
 
-    @abstractmethod
     def refund(self, item) -> None:
         raise Exception("No se puede recibir desde este estado.")
 
-    @abstractmethod
     def cancel(self, item) -> None:
         raise Exception("No se puede devolver desde este estado.")
 
