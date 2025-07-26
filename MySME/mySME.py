3c34561aec7abcb2d081df982d194a46dc8a8ecf
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2025-07-25 22:10:37 -0300

    I've added testing to create both items from console

--- MySME/mySME.py
+++ MySME/mySME.py
@@ -1,4 +1,6 @@
 from Items.supply import Supply
+from Factory_method.item_factory import ItemFactory
+from Items.enums.item_types_enum import ItemTypesEnum
 """
 This module orchestrates the interaction between the different modules on this software.
 It receives commands from the CLI module, executes them either by delegating the responsibility
@@ -9,13 +11,28 @@ to other modules or by itself.
 class MySME:
 
     __supplies: list = []
+    __third_party__services: list = []
+    __item_factory: ItemFactory
+
+    def __init__(self) -> None:
+        self.__item_factory = ItemFactory()
 
     def add_supply(self, product, quantity, metric_unit, petitioner):
 
         self.__supplies.append(
-            Supply(product, quantity, metric_unit, petitioner))
+            self.__item_factory.create_item(ItemTypesEnum.SUPPLY, product, quantity, metric_unit, petitioner))
 
     def show_supply(self):
 
         for supply in self.__supplies:
             print(supply)
+
+    def add_third_services(self, service, provider, petitioner):
+
+        self.__third_party__services.append(
+            self.__item_factory.create_item(ItemTypesEnum.THIRD_PARTY_SERVICES, service, provider, petitioner))
+
+    def show_third_services(self):
+
+        for service in self.__third_party__services:
+            print(service)
