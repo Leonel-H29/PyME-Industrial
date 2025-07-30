16842fe3176c7b982846763fd733d44c71c7f46e
Germán Kousal <germankousal@protonmail.com>
2025-07-30 11:37:49 -0300

    Changes regarding to the second meeting

--- Factory_method/item_factory.py
+++ Factory_method/item_factory.py
@@ -2,7 +2,6 @@ from Items.item import Item
 from Items.supply import Supply
 from Items.third_party_service import ThirdPartyServices
 from Items.enums.item_types_enum import ItemTypesEnum
-from Items.enums.item_status_enum import ItemStatusEnum
 
 
 class ItemFactory:
@@ -15,22 +14,3 @@ class ItemFactory:
             return ThirdPartyServices(*args, **kwargs)
         else:
             raise ValueError(f"Item type '{item_type}' no reconocido")
-
-    @staticmethod
-    def change_item_status(item, new_status: str):
-        try:
-            status_methods = {
-                ItemStatusEnum.QUOTE: item.quote,
-                ItemStatusEnum.ORDER: item.order,
-                ItemStatusEnum.TRANSPORT: item.transport,
-                ItemStatusEnum.RECEIVE: item.receive,
-                ItemStatusEnum.REFUND: item.refund,
-                ItemStatusEnum.CANCEL: item.cancel
-            }
-            status_enum = ItemStatusEnum(new_status)
-            status_method = status_methods[status_enum]
-            status_method()
-        except KeyError:
-            raise ValueError(f"Estado '{new_status}' no reconocido")
-        except ValueError:
-            raise ValueError(f"Estado '{new_status}' no es un estado válido")
