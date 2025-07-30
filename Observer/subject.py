06036391a54e14d1b53d08a0d4b5e626b398826e
Leonel Herrera <leonelherreradkno@gmail.com>
2025-07-30 17:54:36 -0300

    I avoid the duplication of observers
    I've added a condition in order to avoid one observer can be in the list many times.

--- Observer/subject.py
+++ Observer/subject.py
@@ -1,6 +1,7 @@
 from Observer.observer import Observer
 from abc import ABC
 
+
 class Subject(ABC):
 
     __observers: list
@@ -9,7 +10,8 @@ class Subject(ABC):
         self.__observers = []
 
     def add(self, observer: Observer) -> None:
-        self.__observers.append(observer)
+        if observer not in self.__observers:
+            self.__observers.append(observer)
 
     def remove(self, observer: Observer) -> None:
         self.__observers.remove(observer)
@@ -17,7 +19,7 @@ class Subject(ABC):
     def notify(self, item, message):
         for observer in self.__observers:
             observer.update(item, message)
-    
+
     def get_observers(self) -> str:
         observers: str = ""
         for observer in self.__observers:
